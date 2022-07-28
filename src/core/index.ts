091ad9d83889d12acbb0564477345bc1899e9d1a
xiaoliang <xiaoliang@tezign.com>
2022-07-28 18:47:28 +0800

    feat: support extends keyword

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,57 +1,109 @@
 import { TypescriptParser } from "typescript-parser";
-import { TestItem } from "vscode";
+
+// * ---------------------------------------------------------------- inter
 
 // TODO
 type AST_NODE = any;
+type CodeMap = Map<string, string>;
+type InterfaceRelation = {
+  origin: string;
+  extends_from: string;
+}
+
+// * ---------------------------------------------------------------- util
 
 const parser = new TypescriptParser();
 
-export const turnToCode = async (input: string) => {
+/** if input include extends keyword, will return two interfaces' relation */
+const getInterfaceRelation = (input: string): InterfaceRelation[] => {
+  const result: InterfaceRelation[] = [];
+  const arr = input.slice().split(' ');
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 'extends') {
+      result.push({
+        origin: arr[i - 1],
+        extends_from: arr[i + 1]
+      });
+    }
+  }
+  return result;
+}
+
+const getCodeMapByProp = (props: AST_NODE[]) => {
+  const map: CodeMap = new Map();
   let code = "";
+  for (let prop of props) {
+    code = "\n";
+    const { varName, properties } = prop;
+    code += `const ${varName?.toLocaleLowerCase()} = {`
+    for (let i = 0; i < properties?.length; i++) {
+      const { type, name } = properties[i];
+      if (type?.toLowerCase() === "string") {
+        code += `${name}: "",`;
+      } else if (type?.toLowerCase() === "number") {
+        code += `${name}: 0,`;
+      } else if (type?.indexOf("{") > -1 && type?.indexOf("}") > -1) {
+        // TODO: dfs?
+        code += `${name}: ${type},`;
+      } else if (type?.indexOf("[") > -1 && type?.indexOf("]") > -1) {
+        code += `${name}: [],`;
+      } else if (type?.toLowerCase() === "boolean" || type?.toLowerCase() === "false") {
+        code += `${name}: false,`;
+      } else if (type?.toLowerCase() === "true") {
+        code += `${name}: true,`;
+      } else {
+        code += `${name}: {},`;
+      }
+    }
+    code += "};";
+    map.set(varName, code);
+  }
+  return map;
+};
+
+/** For supporting extends keyword */
+const updateCodeMap = (relation: InterfaceRelation[], codeMap: CodeMap): CodeMap => {
+  const newMap = new Map<string, string>(codeMap);
+  newMap.forEach((_, key) => {
+    if (relation.some(item => item.origin === key)) {
+      const extendInterface = relation.find(item => item.origin === key)?.extends_from || '';
+      const originCode = newMap.get(key) || '';
+      const extendCode = newMap.get(extendInterface) || '';
+      const originIndex = originCode?.indexOf('}') || -1;
+      const extendIndex = extendCode?.indexOf('{') || -1;
+      const newCode = (!!extendCode ? originCode?.slice(0, originIndex) : originCode) + (extendIndex < 0 ? '' : extendCode?.slice(extendIndex + 1));
+      newMap.set(key, newCode)
+    }
+  })
+  return newMap
+}
+
+const generateCode = (map: CodeMap): string => {
+  let code = '';
+  map.forEach((value) => {
+    code += `${value}\n`;
+  })
+  return code
+}
+
+// * ---------------------------------------------------------------- main
+
+export const turnToCode = async (input: string) => {
   const ast = await parser.parseSource(input);
-  // const node = ast.declarations[0] as AST_NODE;
   const node = JSON.parse(JSON.stringify(ast.declarations));
   const props = node?.map((item: AST_NODE) => {
     const { name, properties } = item;
     return {
-      pName: name,
+      varName: name,
       properties: properties?.map((item: AST_NODE) => ({ name: item.name, type: item.type })),
     }
   });
 
   const hasNoTsInterface = props.some((item: any) => !item.properties);
-  // if (hasNoTsInterface) throw 'Only support to transform interface to object!';
-  // return hasNoTsInterface;
-  // code += `const ${node.name.toLocaleLowerCase()} = {`;
-  
-  const getProp = (props: AST_NODE[]) => {
-    let code = "";
-    for (let prop of props) {
-      if (!prop) return "";
-      const { pName, properties } = prop;
-      code += `const ${pName.toLocaleLowerCase()} = {`
-      for (let i = 0; i < properties?.length; i++) {
-        const { type, name } = properties[i];
-        if (type?.toLowerCase() === "string") {
-          code += `${name}: "",`;
-        } else if (type?.toLowerCase() === "number") {
-          code += `${name}: 0,`;
-        } else if (type?.indexOf("{") > -1 && type?.indexOf("}") > -1) {
-          // TODO: dfs?
-          code += `${name}: ${type},`;
-        } else if (type?.indexOf("[") > -1 && type?.indexOf("]") > -1) {
-          code += `${name}: [],`;
-        } else if (type?.toLowerCase() === "boolean" || type?.toLowerCase() === "false") {
-          code += `${name}: false,`;
-        } else if (type?.toLowerCase() === "true") {
-          code += `${name}: true,`;
-        } else {
-          code += `${name}: {},`;
-        }
-      }
-      code += "};\n";
-    }
-    return code;
-  };
-  return JSON.parse(JSON.stringify(getProp(props)));
+  if (hasNoTsInterface) throw 'Only support to transform interface to object!';
+  const interface_relation = getInterfaceRelation(input)
+  const codeMap = getCodeMapByProp(props)
+  const newCodeMap = updateCodeMap(interface_relation, codeMap)
+  const code = generateCode(newCodeMap)
+  return code
 };
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,25 +1,5 @@
 import { describe, expect, it } from 'vitest'
 import { turnToCode } from '../src/core';
-import { TypescriptParser } from "typescript-parser";
-const parser = new TypescriptParser();
-
-interface TEST {
-  name: string;
-  age: number;
-  dislike: ["test", "test2"];
-  like: string[] | number[];
-  info: TEST;
-  more: {
-    name: string;
-    age: number;
-    like: string[] | number[];
-    test: {
-      name: string;
-      age: number;
-      like: string[] | number[];
-    };
-  };
-};
 
 const input = `interface TEST {
   name: string;
@@ -27,6 +7,8 @@ const input = `interface TEST {
   dislike: ["test", "test2"];
   like: string[] | number[];
   info: TEST;
+  isSingle: true;
+  isMarried: boolean;
   more: {
     name: string;
     age: number;
@@ -41,10 +23,11 @@ const input = `interface TEST {
 
 const notTs = 'const a = 1';
 
-describe('working', () => {
+describe('working with single situation', () => {
   it("should turn to js object", async () => {
     expect(await turnToCode(input)).toMatchInlineSnapshot(`
-      "const test = {name: \\"\\",age: 0,dislike: [],like: [],info: {},more: {
+      "
+      const test = {name: \\"\\",age: 0,dislike: [],like: [],info: {},isSingle: true,isMarried: false,more: {
           name: string;
           age: number;
           like: string[] | number[];
@@ -58,14 +41,8 @@ describe('working', () => {
     `);
   });
 })
-interface A {
-  a: string
-}
-interface B extends A {
-  b: string
-}
 
-const input_extends = `interface A {
+const input_extends = `interface A extends B {
   a: string;
   c: number;
 }
@@ -76,65 +53,33 @@ interface B {
   f: false;
 }`;
 
-const test = `interface A {
+const input_extends2 = `interface A extends B {
   a: string;
 }`
 
-describe('extends', () => {
+describe('working with extends situation', () => {
   it("should working with extends", async () => {
     expect(await turnToCode(input_extends)).toMatchInlineSnapshot(`
-      "const a = {a: \\"\\",c: 0,};
+      "
+      const a = {a: \\"\\",c: 0,b: \\"\\",d: false,e: true,f: false,};
+
       const b = {b: \\"\\",d: false,e: true,f: false,};
       "
     `);
-  })
-  it("should working with extends", async () => {
-    const ast = await parser.parseSource(test)
-    const node = JSON.parse(JSON.stringify(ast.declarations));    // // const node = ast.declarations;
-    // const props = node?.properties;
-    expect(ast).toMatchInlineSnapshot(`
-      File {
-        "declarations": [
-          InterfaceDeclaration {
-            "accessors": [],
-            "end": 28,
-            "isExported": false,
-            "methods": [],
-            "name": "A",
-            "properties": [
-              PropertyDeclaration {
-                "end": 26,
-                "isOptional": false,
-                "isStatic": false,
-                "name": "a",
-                "start": 16,
-                "type": "string",
-                "visibility": 2,
-              },
-            ],
-            "start": 0,
-          },
-        ],
-        "end": 28,
-        "exports": [],
-        "filePath": "inline.tsx",
-        "imports": [],
-        "resources": [],
-        "rootPath": "/",
-        "start": 0,
-        "usages": [
-          "a",
-        ],
-      }
-    `);
+    expect(await turnToCode(input_extends2)).toMatchInlineSnapshot(`
+      "
+      const a = {a: \\"\\",};
+      "
+    `)
   })
 })
 
-describe('not working', () => {
-  it("should throw error", async () => {
-    expect(await turnToCode(notTs)).toMatchInlineSnapshot(`
-      "const a = {};
-      "
-    `);
-  })
-})
+// describe('not working', () => {
+//   it("should throw error", async () => {
+//     expect(await turnToCode(notTs)).toMatchInlineSnapshot(`
+//       "const a = {};
+
+//       "
+//     `);
+//   })
+// })
